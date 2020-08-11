03d82a4bbede9027ab782bcd30008cffdd16dbac
phishx-devops <pedro.vieira@phishx.io>
2020-08-11 18:37:08 -0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:pedromvieira/phishx.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com/pedromvieira/phishx.github.io.git master:gh-pages
+git push -f git@github.com/phishx-devops/phishx.github.io.git master:gh-pages
 
 cd -
